95f27e3b57776f7754d2e3937078c00d4f9d8f59
Grai <gayathri@gmail.com>
2025-12-13 22:50:03 +0530

    Changes done in pom.xml file

--- user-creation-application/src/main/java/com/app/usercreation/UserCreationApplication.java
+++ user-creation-application/src/main/java/com/app/usercreation/UserCreationApplication.java
@@ -2,9 +2,16 @@ package com.app.usercreation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserCreationApplication {
+public class UserCreationApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserCreationApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserCreationApplication.class, args);
